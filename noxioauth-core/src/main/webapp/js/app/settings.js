4d4d6d4cde46a982fadcf5bf1b18b47d216caeff
Inferno <inferno@localhost.localdomain>
2017-11-15 19:50:14 -0600

    files.

---
+++ noxioauth-core/src/main/webapp/js/app/settings.js
@@ -0,0 +1,26 @@
+"use strict";
+/* global main */
+
+function Settings() {
+  this.defaults();
+  this.downloadUserSettings();
+}
+
+// Sets default values to user settings, this is done initially incase retrieving user settings fails
+Settings.prototype.defaults = function() {
+  this.volume = {
+    master: 0.9,
+    music: 0.5,
+    fx: 0.75
+  };
+};
+
+// Gets saved user settings from server
+Settings.prototype.downloadUserSettings = function() {
+  // @TODO: EYYYYYYYYYYYYyyy
+};
+
+// Saves changed setting to server
+Settings.prototype.saveUserSettings = function() {
+  
+};
